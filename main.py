368ed1a3b450f63bee26c737f8aae3a2351cc836
Allie Quintano <alliequintano@gmail.com>
2021-09-10 16:52:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import pytest
 
+#  The Romans wrote numbers using letters - I, V, X, L, C, D, M.
+#  (notice these letters have lots of straight lines
+#  and are hence easy to hack into stone tablets)
+#
+#  The Kata says you should write a function to
+#  convert from normal numbers to Roman Numerals: eg
+#
+#    1 --> I
+#    10 --> X
+#    7 --> VII
+#    etc.
+#
+#  I = 1 V = 5 X = 10
+#  L = 50 C = 100
+#  D = 500 M = 1000
+#
+#  There is no need to be able to convert numbers larger
+#  than about 3000. (The Romans themselves didn't tend to go any higher)
+#
+# Note that you can't write numerals like "IM" for 999. Wikipedia says:
+# Modern Roman numerals are written by expressing each digit
+# separately starting with the left most digit and skipping any digit
+# with a value of zero. To see this in practice, consider the example
+# of 1990. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC;
+# resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII.
+
 Rules = {
     1000: "M",
     900: "CM",
